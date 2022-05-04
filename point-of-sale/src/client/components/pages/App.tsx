ce1f97b9d9fef966e04584d6b62d4e049cf21497
Sean12121204 <104540306+Sean12121204@users.noreply.github.com>
2022-05-03 21:53:14 -0700

    headers app.tsx

--- point-of-sale/src/client/components/pages/App.tsx
+++ point-of-sale/src/client/components/pages/App.tsx
@@ -105,7 +105,7 @@ App.getInitialProps = async (appContext) => {
     const recipient = query.recipient as string;
     const label = query.label as string;
     const message = query.message || undefined;
-    const host = req?.headers.host || 'localhost:3001';
+    const host = req?.headers.host || 'wndo-pay.vercel.app';
 
     return {
         ...props,
